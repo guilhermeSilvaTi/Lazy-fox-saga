8ae388dfb1069a0f26317e1692b4665ba846742e
Guilherme S Ramos <guilherme_silva_ti@yahoo.com.br>
2023-01-11 20:27:43 -0300

    Save System part 02

--- Lazy Fox Saga Game/Assets/Script/SaveGame/Save.cs
+++ Lazy Fox Saga Game/Assets/Script/SaveGame/Save.cs
@@ -5,17 +5,19 @@ using UnityEngine;
 [System.Serializable]
 public class Save : MonoBehaviour
 {
-    private  int score;
-    private  float musicValue = 0.4f;
-    private  float soundFXValue = 0.5f;
-    private  bool isGyroscope;
- 
-    private  List<PhaseLevel> phaseLevels = new List<PhaseLevel>(20);
+    private int score;
+    private float musicValue = 0.4f;
+    private float soundFXValue = 0.5f;
+    private bool isGyroscope;
+
+    private List<PhaseLevel> phaseLevels = new List<PhaseLevel>(20);
     public Save()
     {
         PhaseLevel phase = new PhaseLevel(false, false, false, false);
         phaseLevels = new List<PhaseLevel>(20) { new PhaseLevel(true, false, false, false), phase, phase, phase, phase, phase, phase, phase, phase, phase, phase, phase, phase, phase, phase, phase, phase, phase, phase, phase };
     }
+    public List<PhaseLevel> GetList(){return phaseLevels; }
+
     public  bool GetPhaseLevel(int value) { return phaseLevels[value].GetIsLevelUnlock(); }
     public  bool GetPhaseCoin(int index, int indexCoin) { return phaseLevels[index].GetGoal(indexCoin); }
     public  void SetPhaseLevel(bool value, int index, bool coin1, bool coin2, bool coin3) { phaseLevels[index] = new PhaseLevel(true, coin1, coin2, coin3); }
--- Lazy Fox Saga Game/Assets/Script/StatesGame.cs
+++ Lazy Fox Saga Game/Assets/Script/StatesGame.cs
@@ -6,11 +6,11 @@ using UnityEngine;
 public class StatesGame : MonoBehaviour
 {
     private static int score;
-    private static float musicValue=0.4f;
-    private static float soundFXValue=0.5f;
+    private static float musicValue = 0.4f;
+    private static float soundFXValue = 0.5f;
     private static bool isGyroscope;
-     private static PhaseLevel phase = new PhaseLevel(false, false, false, false);
-    private static List<PhaseLevel> phaseLevels = new List<PhaseLevel>(20) { new PhaseLevel(true, false, false, false), phase, phase, phase, phase, phase, phase, phase, phase, phase , phase, phase, phase, phase, phase, phase, phase, phase, phase, phase };
+    private static PhaseLevel phase = new PhaseLevel(false, false, false, false);
+    private static List<PhaseLevel> phaseLevels = new List<PhaseLevel>(20) { new PhaseLevel(true, false, false, false), phase, phase, phase, phase, phase, phase, phase, phase, phase, phase, phase, phase, phase, phase, phase, phase, phase, phase, phase };
 
     public static void CallStatesGame()
     {
@@ -19,8 +19,8 @@ public class StatesGame : MonoBehaviour
     }
 
     public static bool GetPhaseLevel(int value) { return phaseLevels[value].GetIsLevelUnlock(); }
-    public static bool GetPhaseCoin(int index,int indexCoin) { return phaseLevels[index].GetGoal(indexCoin); }
-    public static void SetPhaseLevel(bool value, int index,bool coin1, bool coin2, bool coin3) { phaseLevels[index] = new PhaseLevel(true, coin1, coin2, coin3); } 
+    public static bool GetPhaseCoin(int index, int indexCoin) { return phaseLevels[index].GetGoal(indexCoin); }
+    public static void SetPhaseLevel(bool value, int index, bool coin1, bool coin2, bool coin3) { phaseLevels[index] = new PhaseLevel(true, coin1, coin2, coin3); }
 
     public static bool GetIsGyroscope() { return isGyroscope; }
     public static void SetIsGyroscope(bool value) { isGyroscope = value; }
@@ -35,14 +35,31 @@ public class StatesGame : MonoBehaviour
 
     public static void SetSave(Save save)
     {
+        for (int i = 0; i < phaseLevels.Count; i++)
+            save.SetPhaseLevel(phaseLevels[i].GetIsLevelUnlock(), i, phaseLevels[i].GetGoal(0), phaseLevels[i].GetGoal(1), phaseLevels[i].GetGoal(2));
 
+        save.SetIsGyroscope(GetIsGyroscope());
+        save.SetMusic(GetMusic());
+
+        save.SetSoundFX(GetSoundFX());
+
+        save.SetScore(GetScore());
     }
     public static void SetLoad(Save save)
     {
-
+        phaseLevels = save.GetList();
+        SetIsGyroscope(save.GetIsGyroscope());
+        SetMusic(save.GetMusic());
+        SetSoundFX(save.GetSoundFX());
+        SetScore(save.GetScore());
     }
     public static void ResetStatics()
     {
+        score = 0;
+        musicValue = 0.4f;
+        soundFXValue = 0.5f;
+        isGyroscope = false;
+        List<PhaseLevel> phaseLevels = new List<PhaseLevel>(20) { new PhaseLevel(true, false, false, false), phase, phase, phase, phase, phase, phase, phase, phase, phase, phase, phase, phase, phase, phase, phase, phase, phase, phase, phase };
 
     }
 }
